e1ac575e5b778f3bbaaaa5bcba6cabce758afe69
Dave Michelson <daveism@gmail.com>
2016-05-11 20:35:00 -0400

    update for new server

--- js/bcl.js
+++ js/bcl.js
@@ -1,7 +1,7 @@
 BCL = {};
 var BCL  = {
 	createloop:function(aNode,aDistance){
-		urlStr = "http://50.19.41.163:3000/bcl/loop/"+aNode+"/"+aDistance; //54.197.234.215
+		urlStr = "http://107.20.110.87:3000/bcl/loop/"+aNode+"/"+aDistance; //54.197.234.215
 		$('#bLoop').html('Create Another Loop <span class="glyphicon glyphicon-exclamation-sign"></span>');
 	    $.ajax({
 	    	type : "GET",
